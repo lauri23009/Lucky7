aed79c7c07c1bc45a7fa0f2ebe9d4c84fd7b53a2
lauri23009 <lauri23009@student.hamk.fi>
2023-09-07 11:52:16 +0300

    Initial commit

---
+++ src/Lucky7.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class Lucky7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Random random = new Random();
+		
+		int randomNumber1;
+		int randomNumber2;
+		int randomNumber3;
+		
+		
+		randomNumber1 = random.nextInt(11);	
+			System.out.println("Numero 1 = " + randomNumber1);
+			
+		randomNumber2 = random.nextInt(11);	
+			System.out.println("Numero 2 = " + randomNumber2);
+			
+		randomNumber3 = random.nextInt(11);	
+			System.out.println("Numero 3 = " + randomNumber3);
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
